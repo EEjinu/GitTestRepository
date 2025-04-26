565d20f60798aaa8b618d66f559208fd5c998560
Eerang2 <dlwlsdn8022@gmail.com>
2025-04-26 19:33:03 +0900

    test commit

---
+++ src/main/java/green/gittest/TestController.java
@@ -0,0 +1,12 @@
+package green.gittest;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+    public static void main(String[] args) {
+
+
+    }
+}
